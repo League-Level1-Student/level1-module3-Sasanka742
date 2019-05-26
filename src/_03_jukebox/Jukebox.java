2ee98311bdf426b638710ccbaad9ae23abf7301a
League Student <league@iMac-26.attlocal.net>
2019-05-26 11:28:49 -0700

    lvl1_mod3Day2

--- src/_03_jukebox/Jukebox.java
+++ src/_03_jukebox/Jukebox.java
@@ -5,6 +5,9 @@ package _03_jukebox;
  */
 
 
+import java.awt.event.ActionEvent;
+
+import java.awt.event.ActionListener;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.InputStream;
@@ -12,6 +15,7 @@ import java.net.URL;
 
 import javax.swing.Icon;
 import javax.swing.ImageIcon;
+import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.SwingUtilities;
@@ -21,40 +25,68 @@ import javazoom.jl.player.advanced.AdvancedPlayer;
 /*   If you don't have javazoom.jar in your project, you can download it from here: http://bit.ly/javazoom
  *   Right click your project and add it as a JAR (Under Java Build Path > Libraries).*/
 
-public class Jukebox implements Runnable {
-
+public class Jukebox implements Runnable, ActionListener {
+	Song song;
+	static JFrame frame = new JFrame();
+	static JLabel label = loadImage("src/_03_jukebox/futuristic-user-interface-control-concept-4k-hud-stock-footage_bhg_r4l-je_thumbnail-full01.png");
+	
+	static JButton button1 = new JButton("Option One");
+	static JButton button2 = new JButton("Option Two");    	
+	
 	public static void main(String[] args) {
-		Jukebox box = new Jukebox();
-		
+    	
+    	frame.setVisible(true);
+    	frame.add(label);
+    	
+    	label.add(button1);
+    	label.add(button2);
+    	
+    	
 	}
     public void run() {
 
-    	JFrame frame = new JFrame();
-    	JLabel label = loadImage("/Level1-Module3/src/_03_jukebox/Alan Walker - Force my-free-mp3s.com .mp3");
-    	frame.setVisible(true);
-    	frame.add(label);
+    	button1.addActionListener(this);
+    	button2.addActionListener(this);
+    	frame.pack();
 		// 1. Find an mp3 on your computer or on the Internet.
-		// 2. Create a Song object for that mp3
+		// 2. Create a Song object for that mp3 	
 
 		// 3. Play the Song
-
-		/*
+		
+    	
+    	/*
 		 * 4. Create a user interface for your Jukebox so that the user can to
 		 * choose which song to play. You can use a different button for
 		 * each song, or a picture of the album cover. When the button or album
 		 * cover is clicked, stop the currently playing song, and play the one
 		 * that was selected.
 		 */
+    	
+
     }
     
     
+    
 	/* Use this method to add album covers to your Panel. */
-	private JLabel loadImage(String fileName) {
+	private static JLabel loadImage(String fileName) {
 		URL imageURL = getClass().getResource(fileName);
 		Icon icon = new ImageIcon(imageURL);
 		return new JLabel(icon);
 	}
 
+	public void actionPerformed(ActionEvent arg0) {
+		// TODO Auto-generated method stub
+		JButton buttonPressed = (JButton) arg0.getSource();
+		
+		if(arg0.getSource()==button1){
+			song = new Song("/Level1-Module3/src/_03_jukebox/Alan Walker - Force my-free-mp3s.com .mp3");
+		}else if(arg0.getSource()==button2){
+			song = new Song("/Level1-Module3/src/_03_jukebox/Alan Walker - Force.mp3");
+		}
+	}
+	
+	
+	
 }
 
 class Song {
@@ -140,5 +172,7 @@ class Song {
 			return this.getClass().getResourceAsStream(songAddress);
 		}
 	}
+	
+	
 }
 
